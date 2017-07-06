0f2dbc0741567af7e9974440c62c5999d56dee8e
Sam Tholiya <tholiya.sam@outlook.in>
2017-07-06 10:28:51 +0530

    Enhancement: Log messages and function refactor

--- odl/validation_library.go
+++ odl/validation_library.go
@@ -2,12 +2,20 @@ package odl
 
 import (
 	"fmt"
+	"log"
 )
 
 func validateOperation(v interface{}, k string) (warnings []string, errors []error) {
 	operation := v.(string)
+	log.Printf("[INFO] Validating operation %s", operation)
 	if operation == "SET" || operation == "ADD" {
 		return nil, nil
 	}
-	return []string{"Only ADD or SET options are allowed"}, []error{fmt.Errorf("[ERROR] Invalid Operation")}
+	return returnError("Only ADD or SET options are allowed", fmt.Errorf("[ERROR] Invalid Operation"))
+}
+
+func returnError(message string, err error) (warnings []string, errors []error) {
+	var errorVar []error
+	var warningVar []string
+	return append(warningVar, message), append(errorVar, err)
 }
