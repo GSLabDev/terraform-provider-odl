9f54e486e11c9a338847f4117b612e9de269fcd4
Sam Tholiya <tholiya.sam@outlook.in>
2017-07-17 20:09:05 +0530

    Bug Fix: Log messages correction in Test case

--- odl/provider_test.go
+++ odl/provider_test.go
@@ -29,19 +29,19 @@ func TestProvider_impl(t *testing.T) {
 }
 
 func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("SCVMM_SERVER_IP"); v == "" {
-		t.Fatal("SCVMM_SERVER_IP must be set for acceptance tests")
+	if v := os.Getenv("ODL_SERVER_IP"); v == "" {
+		t.Fatal("ODL_SERVER_IP must be set for acceptance tests")
 	}
 
 	if v := os.Getenv("ODL_SERVER_PORT"); v == "" {
-		t.Fatal("SCVMM_SERVER_PORT must be set for acceptance tests")
+		t.Fatal("ODL_SERVER_PORT must be set for acceptance tests")
 	}
 
 	if v := os.Getenv("ODL_SERVER_USER"); v == "" {
-		t.Fatal("SCVMM_SERVER_USER must be set for acceptance tests")
+		t.Fatal("ODL_SERVER_USER must be set for acceptance tests")
 	}
 
 	if v := os.Getenv("ODL_SERVER_PASSWORD"); v == "" {
-		t.Fatal("SCVMM_SERVER_PASSWORD must be set for acceptance tests")
+		t.Fatal("ODL_SERVER_PASSWORD must be set for acceptance tests")
 	}
 }
